b4f1cbe4ee9ea57cc2b95106ce944ecc788bba75
abdulrahman <abdulrahmanazam58@gmail.com>
2024-04-23 13:34:16 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let isFriend = await inquirer.prompt([{
+    name: "friendName",
+    type: "string",
+    message : "Enter your friend name:"
+}]);
+if (isFriend.friendName === "Zia Khan" || isFriend.friendName === "Faisal" || isFriend.friendName === "Ameen"){
+    console.log(`${isFriend.friendName} is your friend.`);
+} else{
+    console.log(`${isFriend.friendName} is not your friend`);
+}
